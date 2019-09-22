0db4bb14961b3b37db8318800e98c6fa0361cddb
PJMantoss <joelptoss@gmail.com>
2019-09-22 19:26:21 +0100

    Change App component to a class component

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import './App.css';
 import TodoItem from './TodoItem';
 import todosData from './todosData';
 
-function App() {
+class App extends React.Component {
 
-  const todoItems = todosData.map(item => <TodoItem key={item.id} item={item} />)
+  constructor(){
+    super()
+    this.state = {
+      todos: todosData
+    }
+  }
+
+  render(){
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} />)
 
   return (
     <div className="App">
       {todoItems}
     </div>
   );
+  }
+
 }
 
 export default App;
